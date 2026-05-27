aa5c2d00df03ac8d4cfb2a4c56ae66ad72717d19
calvinboyerrodima-eng <calvinboyer.rodima@gmail.com>
2026-05-27 12:14:51 -0300

    Update and rename sw,js to sw.js

--- sw.js
+++ sw.js
@@ -16,3 +16,4 @@ self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   event.waitUntil(clients.openWindow(event.notification.data.url));
 });
+// actualización service worker
